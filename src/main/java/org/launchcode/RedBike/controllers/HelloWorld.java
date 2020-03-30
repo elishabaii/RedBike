3d675dbb713d65ba85446fedf00c0ab0117971e5
Elizabeth Ernst <eliernst@gmail.com>
2020-03-30 17:28:19 -0500

    Hello World

---
+++ src/main/java/org/launchcode/RedBike/controllers/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.launchcode.RedBike.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping
+    @ResponseBody
+    public String hello(){
+       return  "Hello World!";
+    }
+
+
+}
