7c905751387a0ad706f3009540e8d54ca6550b20
Elizabeth Ernst <eliernst@gmail.com>
2020-03-30 17:14:43 -0500

    First commit

---
+++ src/main/java/org/launchcode/RedBike/RedBikeApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.RedBike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedBikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedBikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/RedBike/RedBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.RedBike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedBikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
