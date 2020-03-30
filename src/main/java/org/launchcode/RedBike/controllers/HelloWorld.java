d5e44b800bdeb6245214679e699bfa648d895b59
Elizabeth Ernst <eliernst@gmail.com>
2020-03-30 17:34:52 -0500

    Hello World

--- src/main/java/org/launchcode/RedBike/controllers/HelloWorld.java
+++ src/main/java/org/launchcode/RedBike/controllers/HelloWorld.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/* written by Elizabeth Ernst*/
 @Controller
 public class HelloWorld {
 
-    @GetMapping
+    //handles  request at / hello
+    @GetMapping("hello")
     @ResponseBody
     public String hello(){
        return  "Hello World!";
